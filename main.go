2228b744b225abfbef33efce0110d8bd983700f6
Haroon Albar <haroonalbarx@gmail.com>
2024-05-18 13:43:52 +0530

    chore: some notes

--- main.go
+++ main.go
@@ -7,22 +7,29 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+  //parses the form
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successfull")
+	fmt.Fprintf(w, "POST request successfull\n")
+
+  // reads the values from the request
 	name := r.FormValue("name")
 	address := r.FormValue("address")
+
 	fmt.Fprintf(w, "Name = %s\n", name)
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+  //checks the path
 	if r.URL.Path != "/hello" {
 		http.Error(w, "Not found bro 404", http.StatusNotFound)
 		return
 	}
+
+  //checks the method
 	if r.Method != "GET" {
 		http.Error(w, "Not supported method bro 404", http.StatusNotFound)
 		return
